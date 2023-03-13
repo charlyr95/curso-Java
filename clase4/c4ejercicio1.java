4dbf8a2632b7e984f1a81d138f94ba1e18e1b772
charlyr95 <carloscr95r@gmail.com>
2023-03-13 15:31:44 -0300

    last update

--- clase4/c4ejercicio1.java
+++ clase4/c4ejercicio1.java
@@ -5,8 +5,63 @@ Alumno: Carlos Romero
 */
 package ejercicios.clase4;
 
+import java.util.Scanner;
+
 class c4ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        //int[] numeros = {5, 12, 9, -4, -10, 0}; // Le paso un arreglo de enteros desordenado
+        int[] numeros = {}; // Si el array está vacío, entonces que pida los números
+
+        if (numeros.length == 0){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Ingrese números separados por un espacio: ");
+            String input = sc.nextLine();
+            input = input.trim();
+            String[] aux = input.split(" ");
+
+            // Como no puedo redimencionar el array int entonces creo otro array
+            int[] nuevosNumeros = new int[aux.length];
+            for(int i = 0; i < aux.length; i++){
+                nuevosNumeros[i] = Integer.parseInt(aux[i]);
+            }
+
+            // Imprimo los resultados
+            nuevosNumeros = ordenar(nuevosNumeros, true); //true: ascendente || false: descendente
+            imprimir(nuevosNumeros);
+        }
+        else {
+            numeros = ordenar(numeros, true); //true: ascendente || false: descendente
+            imprimir(numeros);
+        }
+    }
+
+
+    public static int[] ordenar(int [] numeros, boolean orden){
+        //orden -> true: ascendente || false: descendente
+        //Aplico el ordenamiento tipo burbuja
+        int n = numeros.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (orden) { // Comparo si es ascendente o descendente
+                    if (numeros[j] > numeros[j + 1]) {
+                        int temp = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = temp;
+                    }
+                } else {
+                    if (numeros[j] < numeros[j + 1]) {
+                        int temp = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        return numeros;
+    }
+    public static void imprimir(int[] numeros){
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
     }
 }
--- clase4/c4ejercicio2.java
+++ clase4/c4ejercicio2.java
@@ -5,8 +5,72 @@ Alumno: Carlos Romero
 */
 package ejercicios.clase4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class c4ejercicio2 {
     public static void main(String[] args) {
+        ArrayList number = new ArrayList<>();
+        String ubicacion = "./ejercicios/clase4/archivo.txt";
+
+        Scanner texto = leerArchivo(ubicacion);
+        ArrayList numeros = listNumber(texto);
+        operacion(numeros, "sumar"); // "sumar" o "multiplicar"
 
     }
+
+    public static Scanner leerArchivo(String ubicacion){
+        try {
+            File archivo = new File(ubicacion);
+            Scanner contenido = new Scanner(archivo);
+            return contenido;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se puede leer el archivo.");
+        }
+        return null;
+    }
+
+    public static ArrayList listNumber(Scanner texto){
+        ArrayList arreglo = new ArrayList<>();;
+        if (texto != null) {
+            while (texto.hasNextLine()) {
+                String data = texto.nextLine();
+                data = data.trim();
+                String[] linea = data.split(" ");
+                for(String num : linea){
+                    arreglo.add(Integer.parseInt(num));
+                }
+            }
+        }
+        return arreglo;
+    }
+
+    public static void operacion(ArrayList lista, String accion){
+        int resultado = 0;
+
+        if (accion.toLowerCase() == "sumar"){
+            for(int i = 0; i < lista.size(); i++){
+                resultado = resultado + (int)lista.get(i);
+            }
+            System.out.println("El resultado de la suma de los números es: " + resultado);
+        } else if (accion.toLowerCase() == "multiplicar") {
+            resultado = 1;
+            for(int i = 0; i < lista.size(); i++){
+                resultado = resultado * (int)lista.get(i);
+            }
+            System.out.println("El resultado de la multiplicación de los números es: " + resultado);
+        } else if (accion.toLowerCase() == "imprimir") {
+            for(int i = 0; i < lista.size(); i++){
+                System.out.print(lista.get(i) + " ");
+            }
+        } else {
+            System.out.println("No se reconoce la acción");
+        }
+
+    }
+
+
 }
