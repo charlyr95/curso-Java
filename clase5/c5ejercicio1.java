51350aa32a35662d3346afdc373da439147ad37f
charlyr95 <104534520+charlyr95@users.noreply.github.com>
2023-03-13 20:33:23 -0300

    update

---
+++ clase5/c5ejercicio1.java
@@ -0,0 +1,12 @@
+/*
+ArgentinaPrograma 4.0
+Curso Inicial Java
+Alumno: Carlos Romero
+*/
+package ejercicios.clase5;
+
+class c5ejercicio1 {
+    public static void main(String[] args) {
+
+    }
+}
